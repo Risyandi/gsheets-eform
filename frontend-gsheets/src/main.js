3df63f796f611bac5e8932f9f6df2262e23c75f3
Risyandi <Risyandi.94@gmail.com>
2021-12-21 22:53:46 +0700

    fix: import external file css

--- frontend-gsheets/src/main.js
+++ frontend-gsheets/src/main.js
@@ -2,6 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// import stylesheets
+import '../src/assets/css/bootstrap.min.css' 
+import '../src/assets/css/now-ui-kit.css' 
+import '../src/assets/demo/demo.css'
+
+// import external library
+// import '../src/assets/js/core/jquery.min.js'
+// import '../src/assets/js/core/popper.min.js'
+// import '../src/assets/js/core/bootstrap.min.js'
+// import '../src/assets/js/plugins/bootstrap-switch.js'
+// import '../src/assets/js/plugins/nouislider.min.js'
+// import '../src/assets/js/plugins/bootstrap-datepicker.js'
+// <!-- <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script> -->
+// import '../src/assets/js/now-ui-kit.js'
+
 Vue.config.productionTip = false
 
 new Vue({
