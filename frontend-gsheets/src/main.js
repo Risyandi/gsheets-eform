a72d71e624d7125732b61b4b7a593630a510f839
Risyandi <Risyandi.94@gmail.com>
2021-12-22 12:27:34 +0700

    fix: update mainjs

--- frontend-gsheets/src/main.js
+++ frontend-gsheets/src/main.js
@@ -3,19 +3,9 @@ import App from './App.vue'
 import router from './router'
 
 // import stylesheets
-import '../src/assets/css/bootstrap.min.css' 
-import '../src/assets/css/now-ui-kit.css' 
-import '../src/assets/demo/demo.css'
-
-// import external library
-// import '../src/assets/js/core/jquery.min.js'
-// import '../src/assets/js/core/popper.min.js'
-// import '../src/assets/js/core/bootstrap.min.js'
-// import '../src/assets/js/plugins/bootstrap-switch.js'
-// import '../src/assets/js/plugins/nouislider.min.js'
-// import '../src/assets/js/plugins/bootstrap-datepicker.js'
-// <!-- <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script> -->
-// import '../src/assets/js/now-ui-kit.js'
+import '../public/assets/css/bootstrap.min.css' 
+import '../public/assets/css/now-ui-kit.css' 
+import '../public/assets/demo/demo.css'
 
 Vue.config.productionTip = false
 
