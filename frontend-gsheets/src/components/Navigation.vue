52bfb1dbdf3da38891e8e4ef607e52184656d545
Risyandi <Risyandi.94@gmail.com>
2021-12-22 12:25:21 +0700

    fix: move and create navigation component

---
+++ frontend-gsheets/src/components/Navigation.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="nav">
+      <router-link to="/" exact>Home</router-link> |
+      <router-link to="/about" exact>About</router-link> |  
+      <router-link to="/web" exact>Website</router-link> | 
+      <router-link to="/form" exact>Form</router-link> |
+      <router-link to="/profile" exact>Profile</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation'
+}
+</script>
+
+<style scoped>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
