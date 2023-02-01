3f35d3fd5a31ed7013a10261ed98f1670618761b
Risyandi <risyandi.94@gmail.com>
2023-02-01 17:02:55 +0700

    fix: update list-form

--- frontend-gsheets/src/views/ListForm.vue
+++ frontend-gsheets/src/views/ListForm.vue
@@ -3,12 +3,12 @@
   <Navigation/>
   <div class="list-form">
     <h1>List Data Form</h1>
-    <div v-for="dataGheets in gsheets.slice(1)" :key="dataGheets.length">
-            <p>{{dataGheets[0]}}</p>
-            <p>{{dataGheets[1]}}</p>
-            <p>{{dataGheets[2]}}</p>
-            <p>{{dataGheets[3]}}</p>
-        </div>
+    <div class="card card-list" v-for="dataGheets in gsheets.slice(1)" :key="dataGheets.length">
+      <div class="card-body">
+        <h5 class="card-title">{{ dataGheets[0] + ' - ' + dataGheets[1] }}</h5>
+        <p class="card-text">{{ dataGheets[2]+ ', ' + dataGheets[3] }}</p>
+      </div>
+    </div>
   </div>
 </div>
 </template>
@@ -27,11 +27,13 @@ export default {
   mounted() {
     let getGsheets = gsheetDataServices.getAllData();
     getGsheets.then(response => {
-      // check response status
+      console.log('Response: ' + response.status);
       if (response.status !== 200) {
-        console.log('Error: ' + response.status);
+        // response status not 200
         this.gsheets = null;
+        console.log(this.gsheets, "values gsheets");
       } else {
+        // response status 200
         this.gsheets = response.data.values;
         console.log(this.gsheets, "values gsheets");
       }
